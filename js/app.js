8462dd7439234a12a05d5da1fadd2f2ea9e17cea
Nicolas Marquez <nicolas.marquezlima@gmail.com>
2025-01-21 15:22:47 -0300

    adicionados alertas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 function alterarStatus(id) {
-    //selecionar o jogo
+    //selecionar o jogo pelo id
     let jogo = document.getElementById (`game-${id}`)
+    //nome do jogo
+    let nome = jogo.querySelector('.dashboard__item__name');
     //selecionar a imagem
     let imagem = jogo.querySelector('.dashboard__item__img');
     //selecionar o botao
@@ -13,10 +15,12 @@ function alterarStatus(id) {
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent =     'Alugar';
+        alert(`O jogo ${nome.textContent} agora esta disponivel`)
     //alugar o jogo
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver'
+        alert(`O jogo ${nome.textContent} agora esta alugado`)
     }
 }
